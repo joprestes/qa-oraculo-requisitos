14d2fd4e04785f9168d0fb8195680ef946c0037b
Joelma Ferreira <joelma.prestes@cwi.com.br>
2025-09-15 23:02:09 -0300

    Mensagem: fix: Atualiza importações nos testes unitários após refatoração

--- tests/test_auxiliares.py
+++ tests/test_auxiliares.py
@@ -6,7 +6,7 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from main import extrair_json_da_resposta
+from graph import extrair_json_da_resposta
 
 class TestFuncoesAuxiliares(unittest.TestCase):
     """
